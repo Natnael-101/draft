35aada08ab1fbc4a411f41e73cdbada80dc3c92a
Abenezer <abenezerbisrat33@gmail.com>
2023-03-27 13:40:56 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -1,6 +1,81 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+/**
+ * get_flags - Extracts any flags from the format string
+ * @format: Pointer to format string
+ * @i: Pointer to index of current character being processed
+ * Return: Integer representing flags
+ */
+int get_flags(const char *format, int *i)
+{
+	/* implementation of get_flags function */
+}
+
+/**
+ * get_width - Extracts the width from the format string
+ * @format: Pointer to format string
+ * @i: Pointer to index of current character being processed
+ * @list: Pointer to argument list
+ * Return: Integer representing width
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	/* implementation of get_width function */
+}
+
+/**
+ * get_precision - Extracts the precision from the format string
+ * @format: Pointer to format string
+ * @i: Pointer to index of current character being processed
+ * @list: Pointer to argument list
+ * Return: Integer representing precision
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	/* implementation of get_precision function */
+}
+
+/**
+ * get_size - Extracts the size modifier from the format string
+ * @format: Pointer to format string
+ * @i: Pointer to index of current character being processed
+ * Return: Integer representing size modifier
+ */
+int get_size(const char *format, int *i)
+{
+	/* implementation of get_size function */
+}
+
+/**
+ * handle_print - Calls the appropriate print function for the current specifier
+ * @format: Pointer to format string
+ * @i: Pointer to index of current character being processed
+ * @list: Pointer to argument list
+ * @buffer: Pointer to buffer for storing printed characters
+ * @flags: Integer representing flags
+ * @width: Integer representing width
+ * @precision: Integer representing precision
+ * @size: Integer representing size modifier
+ * Return: Number of characters printed, or -1 on failure
+ */
+int handle_print(const char *format, int *i, va_list list, char *buffer,
+		int flags, int width, int precision, int size)
+{
+	/* implementation of handle_print function */
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
 
 /**
  * _printf - Printf function
@@ -26,6 +101,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -33,6 +109,7 @@ int _printf(const char *format, ...)
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
